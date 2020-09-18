614e7a09fd4636043e30b8ce1636463ba1cf23be
Miriam <miraragal@gmail.com>
2020-09-18 15:50:27 -0600

    Activity Indicators

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,69 +1,93 @@
-import React, { Component } from 'react';
-import { Text, ScrollView, FlatList } from 'react-native';
-import { Card, ListItem } from 'react-native-elements';
-import { connect} from 'react-redux'; 
-import { baseUrl} from '../shared/baseUrl'; 
+import React, { Component } from "react";
+import { Text, ScrollView, FlatList } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
-const mapStateToProps = state => {
-    return {
-        partners: state.partners
-    }
+const mapStateToProps = (state) => {
+  return {
+    partners: state.partners,
+  };
+};
+
+function Mission() {
+  return (
+    <Card title="Our Mission">
+      <Text>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
+    </Card>
+  );
 }
-
-
-function Mission (){
-    return (
-        <Card
-        title='Our Mission'>
-            <Text>
-            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-            </Text>
-        </Card>
-    )
-}
-
 
 class About extends Component {
-    // No lo necesitamos. Hemos defindo mapStateProps para traer info desde el servidor
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         partners: PARTNERS
-    //     };
-    // }
+  // No lo necesitamos. Hemos defindo mapStateProps para traer info desde el servidor
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         partners: PARTNERS
+  //     };
+  // }
 
-    static navigationOptions={
-        title: 'About Us'
+  static navigationOptions = {
+    title: "About Us",
+  };
+
+  render() {
+    const renderPartner = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }} //importamos imagen desde server
+        />
+      );
+    };
+
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
     }
 
-    render (){
-        const renderPartner = ({item}) => {
-            return (
-                <ListItem 
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}} //importamos imagen desde server
-                />
-            )
-        }
-
+    if(this.props.partners.errMess){
         return (
-
-        <ScrollView>
-           <Mission />
-           <Card
-                title='Community Partners'>
-                    <FlatList
-                data={this.props.partners.partners}// llamamos al state con props.partnes y luego al array
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
-            />
-           
-        </Card>
-
-        </ScrollView>
+            <ScrollView>
+            <Mission />
+            <Card
+                 title='Community Partners'>
+                <Text>{this.props.partners.errMess}</Text>
+            
+         </Card>
+         </ScrollView>
         )
     }
+
+    return (
+      <ScrollView>
+        <Mission />
+        <Card title="Community Partners">
+          <FlatList
+            data={this.props.partners.partners} // llamamos al state con props.partnes y luego al array
+            renderItem={renderPartner}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </Card>
+      </ScrollView>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(About); // exportamos Props
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import { FlatList } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect} from 'react-redux'; 
 import { baseUrl} from '../shared/baseUrl'; 
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -36,6 +37,19 @@ class Directory extends Component {
                 />
             );
         };
+
+        if(this.props.campsites.isLoading){
+            return(
+                <Loading />
+            )
+        }
+        if (this.props.campsites.errMess){
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+                </View>
+            )
+        }
 
         return (
             <FlatList
